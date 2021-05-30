f09c3679e4cb232200b165c0e8ea81064ca5befd
natalia.roales <natalia.roales@tadaima.studio>
2021-05-30 19:57:46 +0200

    feat: Resize input blob image and save it to output directory at container
    Get an input blob image, received through the blob storage trigger, and resize it by with, saving it to output directory at container.

--- BlobTrigger/__init__.py
+++ BlobTrigger/__init__.py
@@ -3,6 +3,8 @@ import logging
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
 import os
+from io import BytesIO
+from PIL import Image
 
 connection_string = os.getenv("AzureWebJobsStorage")
 
@@ -17,7 +19,26 @@ def main(myblob: func.InputStream):
     container_name = "samples-workitems"
     container_client = blob_service_client.get_container_client(container_name)
 
-    width = "width-dir"
+    output_width = 300
     created_md5 = "md5"
-    blob_client = container_client.get_blob_client(f"{width}/{created_md5}")
-    blob_client.upload_blob(myblob, blob_type="BlockBlob")
+
+    output_blob = scaling_by_width(myblob, output_width)
+
+    blob_client = container_client.get_blob_client(f"{output_width}/{created_md5}")
+    blob_client.upload_blob(output_blob, blob_type="BlockBlob")
+
+def scaling_by_width(input_blob: func.InputStream, output_width):
+
+    input_stream = BytesIO(input_blob.read())
+    input_img = Image.open(input_stream)
+    
+    wpercent = (output_width / float(input_img.size[0]))
+    hsize = int((float(input_img.size[1]) * float(wpercent)))
+    
+    resized_image = input_img.resize((output_width, hsize), Image.ANTIALIAS)
+    input_stream.close()
+
+    output_byte_arr = BytesIO()
+    resized_image.save(output_byte_arr, format='PNG')
+    
+    return output_byte_arr.getvalue()
