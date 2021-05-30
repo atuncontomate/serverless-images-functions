371a5c088a9575fe9b4128ec79a235d9504b5f15
natalia.roales <natalia.roales@tadaima.studio>
2021-05-30 13:41:19 +0200

    feat: :construction: Save input image to output directory at container
    Save the input image, received through blob storage trigger, to the output directory at blob container.

--- BlobTrigger/__init__.py
+++ BlobTrigger/__init__.py
@@ -1,9 +1,23 @@
 import logging
 
 import azure.functions as func
+from azure.storage.blob import BlobServiceClient
+import os
 
+connection_string = os.getenv("AzureWebJobsStorage")
 
 def main(myblob: func.InputStream):
+
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
+
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    container_name = "samples-workitems"
+    container_client = blob_service_client.get_container_client(container_name)
+
+    width = "width-dir"
+    created_md5 = "md5"
+    blob_client = container_client.get_blob_client(f"{width}/{created_md5}")
+    blob_client.upload_blob(myblob, blob_type="BlockBlob")
