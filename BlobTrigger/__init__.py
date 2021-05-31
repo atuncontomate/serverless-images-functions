2d526009a2ed364023a581db84250ee799b6aad0
natalia.roales <natalia.roales@tadaima.studio>
2021-05-31 17:55:30 +0200

    feat: Get the current task id and change its status to PROCESSING
    Get the current task id, finding it by filepath, and change its status to PROCESSING.

--- BlobTrigger/__init__.py
+++ BlobTrigger/__init__.py
@@ -20,11 +20,15 @@ def main(myblob: func.InputStream):
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
 
+    db_connection = connect_database()
+    task_id = get_task_id_by_filepath(db_connection, myblob.name)
+    update_task_to_processing(db_connection, task_id)
+
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
     container_name = "samples-workitems"
     container_client = blob_service_client.get_container_client(container_name)
-
+    
     input_filename, extension = get_filename_and_extension(myblob.name)
     input_blob_bytes = BytesIO(myblob.read())
 
@@ -34,11 +38,8 @@ def main(myblob: func.InputStream):
 
         blob_client = container_client.get_blob_client(f"{input_filename}/{output_width}/{created_md5}.{extension}")
         blob_client.upload_blob(output_blob, blob_type="BlockBlob")
-        
+    
     input_blob_bytes.close()
-
-    db_connection = connect_database()
-    insert_filepath(db_connection)  #TODO: This is just a POC.
 
 def get_filename_and_extension(filepath):
 
@@ -70,4 +71,14 @@ def connect_database():
 def insert_filepath(db_connection):
     cursor = db_connection.cursor()
     cursor.execute("""INSERT INTO imagefunctions.tasks(filepath) VALUES ('test')""")
+    db_connection.commit()
+
+def get_task_id_by_filepath(db_connection, filepath):
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT id from imagefunctions.tasks WHERE filepath = '%s'" % (filepath))
+    return cursor.fetchone()[0]
+
+def update_task_to_processing(db_connection, task_id):
+    cursor = db_connection.cursor()
+    cursor.execute("UPDATE imagefunctions.tasks SET Status='PROCESSING' WHERE Id = '%s'" % (task_id))
     db_connection.commit()
